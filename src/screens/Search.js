6b82846fa45ffa2f7c7ab8411d95f02e2df19e8b
Dilara Celepli <72510509+DCELE@users.noreply.github.com>
2022-09-24 22:53:51 +0200

    Update Search.js

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,14 +1,22 @@
-import React, { useState, useMemo, useEffect } from 'react';
-import { View, Text, Incubator } from 'react-native-ui-lib';
+import React, { useState, useMemo, useEffect, useRef } from 'react';
+import { View, Text, Incubator, Image, TouchableOpacity } from 'react-native-ui-lib';
 const { TextField } = Incubator
 import { FlatList } from 'react-native';
 import { searchMovies } from '../services/API';
 import debounce from 'lodash.debounce';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function Search({ navigation }) {
 
     const [page, setPage] = useState(1)
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([])
+    const textFieldRef = useRef()
+
+    const onCancel = () => {
+        setPage(1)
+        setMovies([])
+        textFieldRef.current.value = ""
+    }
 
     const onSearch = async (text) => {
         setPage(1)
@@ -16,7 +24,7 @@ export default function Search({ navigation }) {
             setMovies([])
             return
         }
-        const result = await searchMovies(text)
+        const result = await searchMovies(text, page)
         setMovies(result)
     }
 
@@ -30,14 +38,46 @@ export default function Search({ navigation }) {
         };
     });
 
+    const renderItem = ({ item }) => {
+        return (
+            <View marginV-10>
+                <Preview movie={item} />
+            </View>
+        )
+    }
+
     return (
-        <View useSafeArea>
-            <View >
+        <View useSafeArea margin-10>
+            <View row centerV style={{ borderWidth: 1 }}>
+                <Ionicons style={{ marginHorizontal: 3 }} name='search' size={25} />
                 <TextField
+                    ref={textFieldRef}
+                    style={{ height: 40 }}
                     onChangeText={debouncedResults}
                     placeholder={'Search...'}>
                 </TextField>
+                <TouchableOpacity onPress={onCancel}>
+                    <Ionicons style={{ marginHorizontal: 3 }} name='close' size={25} />
+                </TouchableOpacity>
             </View>
+
+            <FlatList
+                data={movies}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
         </View>
     );
+}
+
+function Preview({ movie }) {
+    return (
+        <View row>
+            <Image marginR-10 aspectRatio={0.7} style={{ height: 120 }} source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }} />
+            <View centerV>
+                <Text text70>{movie.title}</Text>
+                <Text>{movie.release_date}</Text>
+            </View>
+        </View>
+    )
 }

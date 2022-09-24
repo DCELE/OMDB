cda8ffbcd2e2c325afb0b30635fd95fac35732ae
Dilara Celepli <72510509+DCELE@users.noreply.github.com>
2022-09-24 18:38:10 +0200

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,25 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native-ui-lib';
+import { FlatList } from 'react-native';
 import { discoverMovies } from '../services/API';
 
 export default function Home({ navigation }) {
 
     const [movies, setMovies] = useState([])
+    const [page, setPage] = useState(1)
+    const [isLoading, setIsLoading] = useState(false)
 
-    useEffect(() => {
-        discoverMovies(1)
+    const fetchMovies = () => {
+        if (isLoading) return
+        setIsLoading(true)
+        console.log(movies)
+        discoverMovies(page)
             .then(response => {
-                console.log("asdasadsda", response)
+                console.log(response.results)
+                setMovies(prev => [...prev, response.results])
             }).catch(error => {
                 console.log(error)
+            }).finally(() => {
+                setIsLoading(false)
             })
+    }
+
+    useEffect(() => {
+        fetchMovies()
     }, [])
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home</Text>
+        <View useSafeArea style={{ flex: 1, alignItems: 'center' }}>
+            <Text>OMDB</Text>
+            <Text style={{ fontSize: 26, fontWeight: 'bold' }}>Discover Movies</Text>
+            <FlatList
+                data={movies}
+                renderItem={movie}
+                keyExtractor={movie => movie.id}
+            />
+            <Button label={'Load more'} onPress={fetchMovies}></Button>
         </View>
     );
+}
+
+function movie({ item }) {
+    console.log(item)
+
+    return (
+        <Text> Movie {item.id}</Text>
+    )
 }
